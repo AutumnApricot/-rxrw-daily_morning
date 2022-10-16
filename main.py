5e3aafcf6b913ccca81a8466b694d4f8d4f7a4b6
AutumnApricot <115911445+AutumnApricot@users.noreply.github.com>
2022-10-16 13:13:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")+"\n秋杏宝宝，今天要开开心心的哦！"
+  words = requests.get("https://api.shadiao.pro/chp"+"\n秋杏宝宝，今天要开开心心的哦！")
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
